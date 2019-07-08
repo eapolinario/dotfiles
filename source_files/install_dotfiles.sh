f8e833e96c1f527935e3e99044ca5a77718668b1
eduardo apolinario <trickster@eduardos-mbp.lan>
2019-07-07 21:16:28 -0700

    Comment `insta_dotfiles.sh` commands.

--- source_files/install_dotfiles.sh
+++ source_files/install_dotfiles.sh
@@ -2,4 +2,7 @@
 
 set -eux  # defensive bash programming.
 
-ln -s /Users/`whoami`/Dropbox/dotfiles/.tmux.conf ~/.tmux.conf
+# -f flag removes the target destination before creating the symbolic link. We do this to protect against failures (e.g. existent links).
+# -n is to guard against creating a symbolic link inside a directory.
+ln -sfn /Users/`whoami`/Dropbox/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfn /Users/`whoami`/Dropbox/dotfiles/.spacemacs ~/.spacemacs
