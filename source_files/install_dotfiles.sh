14b133f24e62d610c7d67d196152ae7065be2439
eapolinario <eapolinario@lyft.com>
2019-11-21 20:43:18 -0800

    use pwd to pick up the path of dotfiles.

--- source_files/install_dotfiles.sh
+++ source_files/install_dotfiles.sh
@@ -4,6 +4,5 @@ set -eux  # defensive bash programming.
 
 # -f flag removes the target destination before creating the symbolic link. We do this to protect against failures (e.g. existent links).
 # -n is to guard against creating a symbolic link inside a directory.
-ln -sfn /Users/`whoami`/Dropbox/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sfn /Users/`whoami`/Dropbox/dotfiles/.spacemacs ~/.spacemacs
-ln -sfn /Users/`whoami`/Dropbox/org ~/org
+ln -sfn `pwd`/.tmux.conf ~/.tmux.conf
+ln -sfn `pwd`/.spacemacs ~/.spacemacs
