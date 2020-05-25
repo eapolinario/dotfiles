cee298975e5235dec8e27cc531b4dd97a1f1d9db
eduardo apolinario <trickster@eduardos-mbp.lan>
2020-05-25 16:15:14 -0700

    Add "fd" as evil-escape-key-sequence.

--- source_files/.doom.d/config.el
+++ source_files/.doom.d/config.el
@@ -24,7 +24,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-solarized-light)
+
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -56,3 +57,5 @@
   :init
   (map! :leader :prefix "g"
         "s" #'magit-status))
+
+(setq-default evil-escape-key-sequence "fd")
