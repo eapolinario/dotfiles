8b8a170de9224e7231741b673c0ba2868b1163d6
eduardo apolinario <trickster@eduardos-mbp.lan>
2020-05-30 14:39:52 -0700

    Add org-reverse-datetree

--- source_files/.doom.d/packages.el
+++ source_files/.doom.d/packages.el
@@ -45,3 +45,6 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+;; [[file:config.el::file+function my-org-links-file org-reverse-datetree-goto-date-in-file][Org template used to store links needs this]]
+(package! org-reverse-datetree)
