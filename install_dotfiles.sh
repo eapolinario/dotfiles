bd0042027ccc9882dfb5843567231e331a1b5f33
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2021-10-09 19:14:37 -0700

    Remove old-style configuration in favor of stow'd configs

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,32 +2,24 @@
 
 set -eux  # defensive bash programming.
 
-# Get absolute path to directory for this script. This is useful if we want to invoke the script from outside the `/source_files` directory.
-# I copied the solution from https://gist.github.com/tvlooy/cbfbdb111a4ebad8b93e#gistcomment-1650842.
-function abs_script_dir_path {
-    SOURCE=${BASH_SOURCE[0]}
-    while [ -h "$SOURCE" ]; do
-        DIR=$( cd -P $( dirname "$SOURCE") && pwd )
-        SOURCE=$(readlink "$SOURCE")
-        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-    done
-    DIR=$( cd -P $( dirname "$SOURCE" ) && pwd )
-    echo $DIR
-}
+################
+# stow'd files #
+################
+for d in doom-emacs tmux; do
+    stow -nvt ~ $d
+done
 
-DIR=$(abs_script_dir_path $0)
-
-# -f flag removes the target destination before creating the symbolic link. We do this to protect against failures (e.g. existent links).
-# -n is to guard against creating a symbolic link inside a directory.
-for DOTFILE in .tmux.conf .spacemacs .zshrc .zshenv .p10k.zsh; do
-    ln -sfn $DIR/$DOTFILE ~/$DOTFILE
-done;
+#######################
+# end of stow'd files #
+#######################
 
 ################
 # tmux plugins #
 ################
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # TODO do I have to clone plugins or can I rely on https://github.com/tmux-plugins/tpm#installing-plugins ?
 
@@ -43,7 +35,7 @@ if [ ! -d ~/.oh-my-zsh ]; then
 fi
 
 # zsh plugins
-for PLUGIN in zsh-autosuggestions zsh-completions zsh-syntax-highlighting fzf-tab; do
+for PLUGIN in zsh-autosuggestions zsh-completions zsh-syntax-highlighting; do
     if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/$PLUGIN ]; then
         git clone https://github.com/zsh-users/$PLUGIN ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/$PLUGIN
     fi
@@ -51,6 +43,10 @@ done;
 
 if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-256color ]; then
     git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-256color
+fi
+
+if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf-tab ]; then
+    git clone https://github.com/Aloxaf/fzf-tab.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf-tab
 fi
 
 if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
