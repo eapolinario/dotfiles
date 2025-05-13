0f21f741ea493c97b91233f98fc81eeb69173ec3
eduardo apolinario <eapolinario@users.noreply.github.com>
2025-05-13 18:00:13 -0400

    feat(bindings): Add comma-list splitter command and leader key
    Introduce ea/split-comma-separated-list, a helper
    that formats a comma-separated list onto multiple
    lines. The command is mapped to <leader> c S via
    +bindings.el, which is now loaded from config.el.
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

---
+++ doom/+bindings.el
@@ -0,0 +1,43 @@
+;;; +bindings.el --- This is just a list of key bindings to be used in my doom emacs configuration -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Eduardo Apolinario
+;;
+;; Author: Eduardo Apolinario <curupa@gmail.com>
+;; Maintainer: Eduardo Apolinario <curupa@gmail.com>
+;;; Commentary:
+;; This is just a list of key bindings to be used in my doom emacs configuration.
+;;
+;;
+;;; Code:
+
+
+;; Since elisp doesn't have namespaces, we need to prefix our functions with a unique prefix, usually people use their
+;; initials or a short name. In this case, I am using "ea" for Eduardo Apolinario.
+(defun ea/split-comma-separated-list ()
+  "Split comma-separated list into multiple lines."
+  (interactive "*")
+  (save-excursion
+    (let* ((indent (+ (current-indentation) 2)) ; Adjust according to your requirement
+           (list-start (progn (search-forward "(") (point)))
+           (list-end (progn (search-forward ")") (1- (point))))
+           (items (split-string (buffer-substring list-start list-end) "," t " ")))
+      (delete-region list-start list-end)
+      (goto-char list-start)
+      ;; Insert each item on a new line with correct indentation and comma
+      (dolist (item items)
+        (insert (format "\n%s%s," (make-string indent ? ) item)))
+      (newline)
+      ;; Move closing parenthesis to  a new line with correct indentation
+      (forward-char)
+      (delete-char 1)
+      (backward-char 2)
+      (when (not (looking-at ","))
+          (forward-char)
+          (insert ","))
+      (delete-char 1)
+      (insert (format "\n%s)" (make-string (current-indentation) ? ))))))
+
+(map! :leader
+      "c S" #'ea/split-comma-separated-list)
+
+;;; +bindings.el ends here
