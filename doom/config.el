9c6e96bb0cd18435d3251a2d0c01e24a68934bb9
eduardo apolinario <eapolinario@users.noreply.github.com>
2025-05-13 18:30:46 -0400

    chore(appearance): Update theme to Catppuccin with mocha flavor
    Change the default theme to Catppuccin and set
    flavor to mocha.
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- doom/config.el
+++ doom/config.el
@@ -33,11 +33,9 @@
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-opera)
-;; (setq doom-theme 'catppuccin)
-;; (setq catppuccin-flavor 'frappuccino)
-(setq doom-theme 'doom-nord)
+;; `load-theme' function.
+(setq doom-theme 'catppuccin)
+(setq catppuccin-flavor 'mocha) ; latte, frappe, macchiato, mocha
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
