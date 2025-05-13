543f47f1c599cdf4dfbdf3803e2a4331a57f2f99
eduardo apolinario <eapolinario@users.noreply.github.com>
2025-05-13 18:22:00 -0400

    feat(install_dotfiles): Improve plugin installation script
    Refactor the zsh plugin installation logic to use
    an array for plugin definitions. This change
    simplifies the script and adds support for
    additional plugins while maintaining previous
    functionality.
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -60,27 +60,37 @@ if [ ! -d ~/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
 fi
 
-# zsh plugins
-for PLUGIN in zsh-autosuggestions zsh-completions; do
-	if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/$PLUGIN ]; then
-		git clone https://github.com/zsh-users/$PLUGIN ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/$PLUGIN
-	fi
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+plugin_definitions=(
+  "zsh-autosuggestions"
+  "zsh-completions"
+  "zdharma-continuum fast-syntax-highlighting"
+  "chrissicool zsh-256color"
+  "Aloxaf fzf-tab"
+)
+
+for entry in "${plugin_definitions[@]}"; do
+  user="zsh-users"
+  plugin=""
+  read -r first second <<< "$entry"
+  if [[ -n $second ]]; then
+    user="$first"
+    plugin="$second"
+  else
+    plugin="$first"
+  fi
+  target_dir="$ZSH_CUSTOM/plugins/$plugin"
+  if [ ! -d "$target_dir" ]; then
+    git clone "https://github.com/$user/$plugin" "$target_dir"
+  fi
 done
 
-if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting ]; then
-	git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-fi
-
-if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-256color ]; then
-	git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-256color
-fi
-
-if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf-tab ]; then
-	git clone https://github.com/Aloxaf/fzf-tab.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/fzf-tab
-fi
-
-if [ ! -d ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
-	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+theme_user=romkatv
+theme_name=powerlevel10k
+theme_dir="$ZSH_CUSTOM/themes/$theme_name"
+if [ ! -d "$theme_dir" ]; then
+  git clone --depth=1 "https://github.com/$theme_user/$theme_name.git" "$theme_dir"
 fi
 
 ###################
