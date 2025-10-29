bd082dbade65ebbf8f22ca291c099ace18cea2c2
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2025-10-29 10:42:57 -0400

    fix(config): adjust Doom Emacs font sizes for better readability
    Set doom-font to size 13 without weight and increase doom-variable-pitch-font size to 15 for improved clarity.
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- doom/config.el
+++ doom/config.el
@@ -26,8 +26,9 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 13))
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 13)
+      doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 15))
+
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
