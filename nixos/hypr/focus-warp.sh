1d482ff5b106939fb5df7469fbd6bbf83b7d6d7c
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2026-03-09 15:37:18 -0400

    Revert "feat(hypr): warp cursor to focused window on SUPER-[hjkl]"
    This reverts commit e93d47507d055dbd476e537de117ffec290672c6.

--- nixos/hypr/focus-warp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Move focus in given direction and warp cursor to center of new active window
-hyprctl dispatch movefocus "$1"
-read -r cx cy < <(hyprctl -j activewindow | jq -r '[.at[0] + (.size[0] / 2 | floor), .at[1] + (.size[1] / 2 | floor)] | @tsv')
-hyprctl dispatch movecursor "$cx" "$cy"
