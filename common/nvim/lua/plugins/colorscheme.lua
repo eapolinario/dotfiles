4f0d4fe1b61ed99e92fac6223d65cbbcfe0329ec
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2026-05-20 22:36:54 -0400

    feat(nvim): switch colorscheme to catppuccin-macchiato
    Replace kanagawa with catppuccin (transparent background) and update
    LazyVim install fallback to catppuccin/habamax.

--- common/nvim/lua/plugins/colorscheme.lua
+++ common/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,21 @@
--- Use gruvbox as the LazyVim colorscheme
+-- Use Catppuccin as the LazyVim colorscheme.
+-- LazyVim already ships a catppuccin plugin spec with a curated set of
+-- integrations (snacks, flash, neotree, telescope, ...). We only override
+-- the bits we care about so those integrations are preserved.
 return {
-  { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      transparent_background = true,
+    },
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
